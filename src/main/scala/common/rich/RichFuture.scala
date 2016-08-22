ffbc3b0c156c021d7687eef64cd30cbda2df406d
Gal Lalouche <lalouche@google.com>
2016-08-22 16:28:15 +0300

    added RichFuture, with getFailure

---
+++ src/main/scala/common/rich/RichFuture.scala
@@ -0,0 +1,42 @@
+package common.rich
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.Try
+
+object RichFuture {
+  implicit class richFuture[T]($: Future[T])(implicit ec: ExecutionContext) {
+    // gives better error message when the filter fails
+    def filterWith(p: T => Boolean, message: String): Future[T] = filterWithMessage(p, e => message)
+    def filterWithStacktrace(p: T => Boolean, message: String = "Failed filter") = {
+      val ex = new NoSuchElementException(message)
+      ex.setStackTrace(ex.getStackTrace drop 1)
+      $.flatMap(e => if (p(e)) $ else Future failed ex)
+    }
+    // implicits suck with overloads it seems
+    def filterWithMessage(p: T => Boolean, message: T => String): Future[T] = $.flatMap(e =>
+      if (p(e))
+        $
+      else
+        Future failed new NoSuchElementException(message(e)))
+    def get: T = Await.result($, Duration.Inf)
+    def getFailure: Throwable = {
+      Await.ready($, Duration.Inf)
+      val triedT: Try[T] = $.value.get
+      try {
+        triedT.failed.get
+      } catch {
+        case e: UnsupportedOperationException => throw new UnsupportedOperationException(s"Expected failure but was success <${triedT.get}>")
+      }
+    }
+    // like recover, but doesn't care about the failure
+    def orElse(t: => T): Future[T] = $.recover { case e => t }
+    // implicits suck with overloads it seems
+    def orElseTry(t: => Future[T]): Future[T] = $.recoverWith { case e => t }
+    def onEnd[S](f: => Future[S]): Future[S] = $.flatMap(e => f)
+  }
+  implicit class richOptionFuture[T]($: Future[Option[T]])(implicit ec: ExecutionContext) {
+    def ifNone(t: => T): Future[T] = $.map(_ getOrElse t)
+    def ifNoneTry(t: => Future[T]): Future[T] = $.flatMap(_ map Future.successful getOrElse t)
+  }
+}
---
+++ src/test/scala/common/rich/RichFutureTest.scala
@@ -0,0 +1,149 @@
+package common.rich
+
+import common.AuxSpecs
+import common.rich.RichFuture._
+import org.scalatest.FreeSpec
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.util.Try
+
+class RichFutureTest extends FreeSpec with AuxSpecs {
+  val success: Future[Int] = Future successful 1
+  val failure: Future[Int] = Future failed new Exception("Derp")
+  def shouldFail(f: Future[Any]) = {
+    Await.ready(f, Duration.Inf)
+    val t = f.value.get
+    t.isFailure shouldReturn true
+  }
+  "RichFuture" - {
+    "get" - {
+      "success" in {
+        val f = Future {
+          Thread.sleep(10)
+          5
+        }
+        f.get shouldReturn 5
+      }
+      "failure" in {
+        val f = Future {
+          Thread.sleep(10)
+          throw new Exception()
+        }
+        an[Exception] should be thrownBy f.get
+
+      }
+    }
+    "get exception" - {
+      "when success should throw" in {
+        val f = Future[Int](1)
+        an[UnsupportedOperationException] should be thrownBy f.getFailure
+      }
+      "when failure should return the error" in {
+        val exception = new IndexOutOfBoundsException("Foobar")
+        val f = Future[Int]{throw exception}
+        val failure = f.getFailure
+        failure.getClass.asInstanceOf[Class[Any]] shouldReturn exception.getClass.asInstanceOf[Class[Any]]
+        failure.getMessage shouldReturn exception.getMessage
+      }
+    }
+    "filterWith" - {
+      "success" in {
+        success.filterWith(_ > 0, "Doesn't matter").get shouldReturn 1
+      }
+      "failure" in {
+        val f = success.filterWith(_ < 0, "Failure message")
+        shouldFail(f)
+        f.value.get.failed.get.getMessage shouldReturn "Failure message"
+      }
+      "stacktrace" in {
+        def foo: Future[Any] = {
+          def bar = {
+            def baz = {
+              def qux = {
+                Future.apply(false).filterWithStacktrace(identity, "Foobar")
+              }
+              qux
+            }
+            baz
+          }
+          bar
+        }
+        val f = foo
+        val stackTrace = Try(f.get).failed.get.getStackTrace
+        stackTrace zip List("qux", "baz", "bar", "foo") map (e => e._1.toString contains e._2) forall identity shouldReturn true
+      }
+    }
+    "orElse" - {
+      "success" in {
+        success.orElse(???).get shouldReturn 1
+      }
+      "failure" in {
+        failure.orElse(10).get shouldReturn 10
+      }
+    }
+    "orElseTry" - {
+      "success" in {
+        success.orElseTry(???).get shouldReturn 1
+      }
+      "failure" - {
+        "to success" in {
+          failure.orElseTry(Future successful 10).get shouldReturn 10
+        }
+        "to failure" in {
+          shouldFail(failure.orElseTry(failure))
+        }
+      }
+    }
+    "onEnd" - {
+      "success" in {
+        success.onEnd(Future successful 10).get shouldReturn 10
+      }
+      "failure" - {
+        val f: Future[Int] = failure.onEnd(Future successful 10)
+        an[Exception] should be thrownBy f.get
+      }
+      "in order" in {
+        val list = new ListBuffer[Int]
+        def add(i: Int) = {
+          Future(list.+=(i))
+        }
+        add(0).onEnd(add(1)).onEnd(add(2)).get
+        list.toList shouldReturn List(0, 1, 2)
+      }
+    }
+  }
+  "RichOptionFuture" - {
+    val success: Future[Option[Int]] = Future successful Some(1)
+    val failure: Future[Option[Int]] = Future failed new Exception("Derp")
+    val none: Future[Option[Int]] = Future successful None
+    "ifNone" - {
+      "success" - {
+        "some" in {
+          success.ifNone(???).get shouldReturn 1
+        }
+        "none" in {
+          none.ifNone(10).get shouldReturn 10
+        }
+      }
+      "failure" in {
+        shouldFail(failure.ifNone(10))
+      }
+    }
+    "ifNoneTry" - {
+      "success" - {
+        "some" in {
+          success.ifNoneTry(???).get shouldReturn 1
+        }
+        "none" in {
+          none.ifNoneTry(Future successful 10).get shouldReturn 10
+        }
+      }
+      "failure" in {
+        shouldFail(failure.ifNoneTry(Future successful 10))
+      }
+    }
+  }
+}
