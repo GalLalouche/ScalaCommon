1774faae3042dab833a4a003302e396cf3e3b121
Gal PC <glpkmtg@gmail.com>
2016-09-24 20:34:16 +0300

    Removed either in RichOption
    This is already handled by toRight/toLeft

--- src/main/scala/common/rich/primitives/RichOption.scala
+++ src/main/scala/common/rich/primitives/RichOption.scala
@@ -7,7 +7,6 @@ object RichOption {
     // throws a better detailed exception when trying to access None
     def getOrThrow(errorMessage: String): T = getOrThrow(new NoSuchElementException(errorMessage))
     def getOrThrow(t: => Throwable): T = toTry(t).get
-    def either[S](b: => S): Either[S, T] = Either.cond($.isDefined, $.get, b)
     def toTry(t: => Throwable): Try[T] = $ match {
       case Some(e) => Success(e)
       case None => Failure(t)
