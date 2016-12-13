256fe2cb178c888ee348d0e2ec56bfb45f06c759
gal_lalouche <glpkmtg@gmail.com>
2016-12-13 21:27:11 +0200

    More monads!

--- src/main/scala/common/rich/func/MoreMonadPlus.scala
+++ src/main/scala/common/rich/func/MoreMonadPlus.scala
@@ -9,4 +9,16 @@ object MoreMonadPlus {
     override def point[A](a: => A): Seq[A] = Seq(a)
     override def empty[A]: Seq[A] = Seq()
   }
+  implicit object TraversableMonadPlus extends MonadPlus[Traversable] {
+    override def bind[A, B](fa: Traversable[A])(f: A => Traversable[B]): Traversable[B] = fa flatMap f
+    override def plus[A](a: Traversable[A], b: => Traversable[A]): Traversable[A] = a ++ b
+    override def point[A](a: => A): Traversable[A] = Traversable(a)
+    override def empty[A]: Traversable[A] = Traversable()
+  }
+  implicit object IterableMonadPlus extends MonadPlus[Iterable] {
+    override def bind[A, B](fa: Iterable[A])(f: A => Iterable[B]): Iterable[B] = fa flatMap f
+    override def plus[A](a: Iterable[A], b: => Iterable[A]): Iterable[A] = a ++ b
+    override def point[A](a: => A): Iterable[A] = Iterable(a)
+    override def empty[A]: Iterable[A] = Iterable()
+  }
 }
