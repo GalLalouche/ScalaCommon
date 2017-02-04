80b7e4ed5d2834651ff2b2dd4c8444e5e5b5314e
gal_lalouche <glpkmtg@gmail.com>
2017-02-04 13:07:21 +0200

    Made Seq Traversable

--- src/main/scala/common/rich/func/MoreTraverse.scala
+++ src/main/scala/common/rich/func/MoreTraverse.scala
@@ -3,11 +3,19 @@ package common.rich.func
 import scalaz.syntax.ToApplicativeOps
 import scalaz.{Applicative, Traverse}
 
-object MoreTraverse {
-  implicit object TraversableTraverse extends Traverse[Traversable] with ToApplicativeOps {
-    override def traverseImpl[G[_], A, B](fa: Traversable[A])(f: (A) => G[B])(implicit evidence: Applicative[G]): G[Traversable[B]] =
-      fa.foldLeft(evidence.point(fa.genericBuilder[B])) {
-        (builder, element) => evidence.apply2(builder, f(element))(_ += _)
+object MoreTraverse extends ToApplicativeOps {
+  // TODO remove code duplication with CanBuildFrom
+  implicit object TraversableTraverse extends Traverse[Traversable] {
+    override def traverseImpl[G[_]: Applicative, A, B](fa: Traversable[A])(f: A => G[B]): G[Traversable[B]] =
+      fa.foldLeft(implicitly[Applicative[G]].point(fa.genericBuilder[B])) {
+        (builder, element) => implicitly[Applicative[G]].apply2(builder, f(element))(_ += _)
       }.map(_.result)
+  }
+  implicit object SeqTraverse extends Traverse[Seq] {
+    override def traverseImpl[G[_]: Applicative, A, B](fa: Seq[A])(f: A => G[B]) = {
+      fa.foldLeft(implicitly[Applicative[G]].point(fa.genericBuilder[B])) {
+        (builder, element) => implicitly[Applicative[G]].apply2(builder, f(element))(_ += _)
+      }.map(_.result)
+    }
   }
 }
