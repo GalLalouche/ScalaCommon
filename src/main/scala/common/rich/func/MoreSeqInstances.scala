c9f375da7db6622abe7561320a5cf88c6a8f6773
Gal Lalouche <lalouche@google.com>
2017-09-06 16:59:48 +0300

    Combined Traverse and MonadPlus instances
    This fixes some implicit collision bugs since every MonadPlus is a Functor but
    so is every Traverse.
    Also had to fix weird ToFunctorOps issue in RichTraversableOnce.

--- src/main/scala/common/rich/func/MoreSeqInstances.scala
+++ src/main/scala/common/rich/func/MoreSeqInstances.scala
@@ -5,14 +5,14 @@ import scalaz.syntax.ToApplicativeOps
 import scalaz.{Applicative, MonadPlus, Monoid, Traverse}
 
 trait MoreSeqInstances {
-  implicit object SeqMonadPlus extends MonadPlus[Seq] {
+  implicit object SeqMonadPlus extends MonadPlus[Seq] with Traverse[Seq]
+      with ToApplicativeOps {
     override def bind[A, B](fa: Seq[A])(f: A => Seq[B]) = fa flatMap f
     override def point[A](a: => A) = Seq(a)
     override def empty[A] = Seq.empty
     override def plus[A](a: Seq[A], b: => Seq[A]) = a ++ b
-  }
-  // TODO handle code duplication
-  implicit object SeqTraverse extends Traverse[Seq] with ToApplicativeOps {
+
+    // TODO handle code duplication
     override def traverseImpl[G[_]: Applicative, A, B](fa: Seq[A])(f: A => G[B]): G[Seq[B]] = {
       fa.foldLeft(Applicative[G].point(fa.genericBuilder[B])) {
         (builder, element) => Applicative[G].apply2(builder, f(element))(_ += _)
