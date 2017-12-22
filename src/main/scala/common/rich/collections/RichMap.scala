8b7e775268ce415bd10fe69f69991a209acdad8f
Gal PC <glpkmtg@gmail.com>
2017-12-22 16:06:51 +0200

    RichMap.upsert

--- src/main/scala/common/rich/collections/RichMap.scala
+++ src/main/scala/common/rich/collections/RichMap.scala
@@ -1,18 +1,19 @@
 package common.rich.collections
 
 import scala.language.higherKinds
+import scalaz.syntax.ToSemigroupOps
 import scalaz.{Plus, Semigroup}
 
 object RichMap {
-  implicit class richSemigroupMap[K, V: Semigroup]($: Map[K, V]) {
+  implicit class richSemigroupMap[K, V: Semigroup]($: Map[K, V]) extends ToSemigroupOps {
     def merge(other: Map[K, V]): Map[K, V] = {
-      def merge(smallMap: Map[K, V], largeMap: Map[K, V]): Map[K, V] =
-        smallMap.foldLeft(largeMap)((agg, e) =>
-          agg.updated(e._1, agg.get(e._1).map(Semigroup[V].append(_, e._2)).getOrElse(e._2)))
-      if ($.size > other.size) merge(other, $) else merge($, other)
+      def aux(smallMap: Map[K, V], largeMap: Map[K, V]): Map[K, V] = smallMap.foldLeft(largeMap)(_ upsert _)
+      if ($.size > other.size) aux(other, $) else aux($, other)
     }
+    def upsert(kv: (K, V)): Map[K, V] = upsert(kv._1, kv._2)
+    def upsert(k: K, v: => V): Map[K, V] = $.updated(k, $.get(k).fold(v)(_ |+| v))
     def mergeIntersecting(other: Map[K, V]): Map[K, V] =
-      $.filterKeys(other.contains).map(e => (e._1, Semigroup[V].append(e._2, other(e._1))))
+      $.filterKeys(other.contains).map(e => (e._1, e._2 |+| other(e._1)))
   }
   implicit class richPlusMap[K, V[_] : Plus, A]($: Map[K, V[A]]) {
     private val asSemigroup = richSemigroupMap($)(Plus[V].semigroup)
