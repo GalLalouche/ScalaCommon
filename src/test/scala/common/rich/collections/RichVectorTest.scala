c504f47a9006b5a0607731a230c5bd54b1676fae
Gal Lalouche <lalouche@google.com>
2018-01-18 12:08:03 +0200

    Fixed commented out RichVector tests

--- src/test/scala/common/rich/collections/RichVectorTest.scala
+++ src/test/scala/common/rich/collections/RichVectorTest.scala
@@ -1,5 +1,7 @@
 package common.rich.collections
 
+import java.lang.Math.abs
+
 import common.AuxSpecs
 import common.rich.collections.RichVector._
 import org.scalacheck.Gen
@@ -9,59 +11,53 @@ import org.scalatest.prop.PropertyChecks
 import scala.util.Random
 
 class RichVectorTest extends PropSpec with PropertyChecks with AuxSpecs {
-  def randomNumber = (Random.nextDouble * 1000) - 1000
+  private def randomNumber = (Random.nextDouble * 1000) - 1000
 
-  def vectors = for (n <- Gen.choose(2, 5)) yield {
-    Vector.fill(n)(randomNumber)
-  }
-  def vectorPairs = for (n <- Gen.choose(2, 5)) yield {
-    Vector.fill(n)(randomNumber) -> Vector.fill(n)(randomNumber)
-  }
-  property("v should have unique entries") {
-    forAll(vectors) {v =>
-      v.toSet.size shouldReturn v.size
-    }
-  }
+  private def vectors = Gen.choose(2, 100).map(Vector.fill(_)(randomNumber))
+  private def vectorPairs =
+    Gen.choose(2, 100).map(n => Vector.fill(n)(randomNumber) -> Vector.fill(n)(randomNumber))
+  private def scalars = Gen.choose(-1000.0, 1000.0)
 
   property("self dot should equal magnitude squared") {
-    forAll(vectors) {v =>
+    forAll(vectors) { v =>
       val mag = v.magnitude
-      v.dot(v) shouldBeApproximately mag * mag
+      v dot v shouldBeApproximately mag * mag
     }
   }
 
   property("A vector should have a cosine similarity of 1.0 to itself") {
-    forAll(vectors) {v =>
-      v.cosineSimilarityTo(v) shouldBeApproximately 1.0
+    forAll(vectors) { v =>
+      v cosineSimilarityTo v shouldBeApproximately 1.0
     }
   }
 
   property("a unit vector's magnitude should be 1") {
-    forAll(vectors) {v =>
+    forAll(vectors) { v =>
       v.toUnit.magnitude shouldBeApproximately 1.0
     }
   }
 
   property("a unit vector's cosine similarity to its original vector should be 1") {
-    forAll(vectors) {v =>
-      v.toUnit.cosineSimilarityTo(v) shouldBeApproximately 1.0
+    forAll(vectors) { v =>
+      v.toUnit cosineSimilarityTo v shouldBeApproximately 1.0
     }
   }
 
   property("dot should be symmetric") {
-    forAll(vectorPairs) {case (u, v) =>
+    forAll(vectorPairs) { case (u, v) =>
       v.dot(u) shouldBeApproximately u.dot(v)
     }
   }
 
   property("Cosine similarity should be symmetric") {
-    forAll(vectorPairs) {case (u, v) =>
+    forAll(vectorPairs) { case (u, v) =>
       v.cosineSimilarityTo(u) shouldBeApproximately u.cosineSimilarityTo(v)
     }
   }
 
+  // Technically if u == v then this isn't true, but that should never happen.
   property("projection should be of a lesser magnitude than the original vector") {
-    forAll(vectorPairs) {case (u, v) =>
+    forAll(vectorPairs) { case (u, v) =>
       v.projectionOn(u).magnitude should be < v.magnitude
     }
   }
@@ -69,35 +65,37 @@ class RichVectorTest extends PropSpec with PropertyChecks with AuxSpecs {
   property("the projection of a vector should have a Cosine similarity of 1 with vector") {
     forAll(vectorPairs) {
       case (u, v) =>
-        v.projectionOn(u).cosineSimilarityTo(u) shouldBeApproximately 1.0
+        v projectionOn u cosineSimilarityTo u shouldBeApproximately 1.0
     }
   }
 
   property("The anti vector should be perpendicular to the vector") {
     forAll(vectorPairs) {
       case (u, v) =>
-        v.antiVector(u).cosineSimilarityTo(u) shouldBeApproximately 0.0
+        v antiVector u cosineSimilarityTo u shouldBeApproximately 0.0
     }
   }
 
   property("Cosine similarity should be non-negative") {
     forAll(vectorPairs) {
       case (u, v) =>
-        v.cosineSimilarityTo(u) should be >= 0.0
+        v cosineSimilarityTo u should be >= 0.0
     }
   }
-  //TODO(fix)
-  //
-  //	"magnitude" should "return the size of the vector" in {
-  //		List(1, 0, 0).magnitude shouldReturn 1
-  //		List(1, 2, 3).magnitude shouldReturn Math.sqrt(14)
-  //	}
-  //
-  //	"unit vector" should "return a normalized vector" in {
-  //		List.fill(100)(Random.nextInt).map(_.toDouble).unitVector.magnitude isRoughly 1 shouldReturn true
-  //	}
-  //
-  //	"percentile" should "return percentile" in {
-  //		List(1, 2, 3).percentile(0.5) shouldReturn 2
-  //	}
+
+  property("Magnitude should ignore sign") {
+    forAll(vectors) { v =>
+      v.magnitude shouldReturn v.map(-_).magnitude
+    }
+  }
+  property("Magnitude is linear: multiplication by scalar") {
+    forAll(vectors, scalars) { (v, s) =>
+      v.*(s).magnitude shouldBeApproximately abs(s) * v.magnitude
+    }
+  }
+  property("Magnitude is linear: addition") {
+    forAll(vectorPairs) { case (v1, v2) =>
+      (v1 + v2).magnitude shouldBeApproximately (v1.magnitude + v2.magnitude)
+    }
+  }
 }
