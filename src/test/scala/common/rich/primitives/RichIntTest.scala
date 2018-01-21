5fb7239924cdfa112ce0cb5c5cb84725613d58d5
Gal Lalouche <lalouche@google.com>
2018-01-21 09:56:52 +0200

    RichIntTest now uses FreeSpec

--- src/test/scala/common/rich/primitives/RichIntTest.scala
+++ src/test/scala/common/rich/primitives/RichIntTest.scala
@@ -2,10 +2,10 @@ package common.rich.primitives
 
 import common.AuxSpecs
 import common.rich.primitives.RichInt._
-import org.scalatest.FlatSpec
+import org.scalatest.FreeSpec
 
-class RichIntTest extends FlatSpec with AuxSpecs {
-  "factorial" should "work with small numbers" in {
+class RichIntTest extends FreeSpec with AuxSpecs {
+  "factorial works with small numbers" in {
     0.factorial shouldReturn 1
     1.factorial shouldReturn 1
     2.factorial shouldReturn 2
@@ -14,54 +14,54 @@ class RichIntTest extends FlatSpec with AuxSpecs {
     5.factorial shouldReturn 120
   }
 
-  "choose" should "work with small numbers" in {
-    5 choose 2 shouldReturn 10
-    10 choose 1 shouldReturn 10
-    100 choose 0 shouldReturn 1
-  }
+  "choose" - {
+    "works with small numbers" in {
+      5 choose 2 shouldReturn 10
+      10 choose 1 shouldReturn 10
+      100 choose 0 shouldReturn 1
+    }
 
-  it should "work with binomial complement" in {
-    5 choose 5 shouldReturn 1
-    5 choose 4 shouldReturn 5
-    5 choose 3 shouldReturn 10
-  }
+    "works with binomial complement" in {
+      5 choose 5 shouldReturn 1
+      5 choose 4 shouldReturn 5
+      5 choose 3 shouldReturn 10
+    }
 
-  it should "work with slightly larger numbers" in {
-    6 choose 3 shouldReturn 20
-  }
-
-  "power" should "return 1 when upping to zero" in {
-    4 exp 0 shouldReturn 1
-  }
-
-  it should "return 0 when 0 is times something" in {
-    0 exp 4 shouldReturn 0
-  }
-
-  it should "throw an exception for 0 ^ 0" in {
-    an[IllegalArgumentException] should be thrownBy {
-      0 exp 0
+    "works with slightly larger numbers" in {
+      6 choose 3 shouldReturn 20
     }
   }
 
-  it should "raise to the power" in {
-    2 exp 4 shouldReturn 16
+  "power" - {
+    "returns 1 when upping to zero" in {
+      4 exp 0 shouldReturn 1
+    }
+    "returns 0 when 0 is times something" in {
+      0 exp 4 shouldReturn 0
+    }
+    "throws an exception for 0 ^ 0" in {
+      an[IllegalArgumentException] should be thrownBy {
+        0 exp 0
+      }
+    }
+    "raises to the power" in {
+      2 exp 4 shouldReturn 16
+    }
+    "works for negative numbers in base" in {
+      -1 exp 0 shouldReturn 1
+      -1 exp 1 shouldReturn -1
+      -1 exp 2 shouldReturn 1
+      -1 exp 3 shouldReturn -1
+    }
   }
 
-  it should "work for negative numbers in base" in {
-    -1 exp 0 shouldReturn 1
-    -1 exp 1 shouldReturn -1
-    -1 exp 2 shouldReturn 1
-    -1 exp 3 shouldReturn -1
-  }
-
-  "permutation choose" should "work with small numbers" in {
+  "permutation works with small numbers" in {
     4 perm 2 shouldReturn 12
     10 perm 3 shouldReturn 720
     5 perm 5 shouldReturn 120
   }
 
-  "primes" should "return a stream of primes" in {
+  "primes returns a stream of primes" in {
     RichInt.primes.take(10).toList shouldReturn List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
   }
 }
