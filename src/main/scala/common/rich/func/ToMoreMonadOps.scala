8551a7f285b052e05e42305ade535043063fbccf
Gal Lalouche <lalouche@google.com>
2018-01-25 20:12:20 +0200

    Replace for with parens with for with braces

--- src/main/scala/common/rich/func/ToMoreMonadOps.scala
+++ src/main/scala/common/rich/func/ToMoreMonadOps.scala
@@ -7,9 +7,9 @@ import scalaz.syntax.ToMonadOps
 trait ToMoreMonadOps extends ToMonadOps {
   implicit class toMoreOptionMonadPlusOps[A, F[_]: Monad]($: F[Option[A]]) {
     private def pure[B](e: B) = Monad.apply.pure(e)
-    def mFilterOpt(p: A => F[Boolean]): F[Option[A]] = for (
-      e <- $;
-      predValue <- e.fold(pure(true))(p);
-      result <- if (predValue) $ else pure(None)) yield result
+    def mFilterOpt(p: A => F[Boolean]): F[Option[A]] = for {
+      e <- $
+      predValue <- e.fold(pure(true))(p)
+      result <- if (predValue) $ else pure(None)} yield result
   }
 }
