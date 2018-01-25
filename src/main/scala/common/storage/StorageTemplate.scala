c7b1bbd9585b7132f453781b3ebb7510e5e3974f
Gal Lalouche <lalouche@google.com>
2018-01-25 20:09:48 +0200

    <*ByName

--- src/main/scala/common/storage/StorageTemplate.scala
+++ src/main/scala/common/storage/StorageTemplate.scala
@@ -2,20 +2,18 @@ package common.storage
 
 import scala.concurrent.{ExecutionContext, Future}
 import scalaz.std.FutureInstances
-import scalaz.syntax.ToFunctorOps
+import scalaz.syntax.{ToBindOps, ToFunctorOps}
 
 /** Provides overrides since the TableUtils trait can't have implicit parameters. */
 abstract class StorageTemplate[Key, Value](implicit ec: ExecutionContext) extends Storage[Key, Value]
-    with ToFunctorOps with FutureInstances {
+    with ToFunctorOps with ToBindOps with FutureInstances {
   /** If a previous value exists, override it. */
   protected def internalDelete(k: Key): Future[_]
   protected def internalForceStore(k: Key, v: Value): Future[_]
 
-  override def forceStore(k: Key, v: Value): Future[Option[Value]] =
-    load(k).flatMap(existing => internalForceStore(k, v).>|(existing))
+  override def forceStore(k: Key, v: Value): Future[Option[Value]] = load(k) `<*ByName` internalForceStore(k, v)
   override def store(k: Key, v: Value): Future[Unit] = storeMultiple(List(k -> v))
   override def mapStore(k: Key, f: Value => Value, default: => Value): Future[Option[Value]] =
     load(k).flatMap(v => forceStore(k, v.fold(default)(f)))
-  override def delete(k: Key): Future[Option[Value]] =
-    for (existing <- load(k); _ <- internalDelete(k)) yield existing
+  override def delete(k: Key): Future[Option[Value]] = load(k) `<*ByName` internalDelete(k)
 }
