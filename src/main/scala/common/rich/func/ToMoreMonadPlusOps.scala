bf22e0486508e5fa79d34b2d201d25031bb38c21
gal_lalouche <glpkmtg@gmail.com>
2018-02-17 14:05:40 +0200

    MonadPlus.uniqueBy

--- src/main/scala/common/rich/func/ToMoreMonadPlusOps.scala
+++ src/main/scala/common/rich/func/ToMoreMonadPlusOps.scala
@@ -2,6 +2,7 @@ package common.rich.func
 
 import common.rich.RichT._
 
+import scala.collection.mutable
 import scala.language.higherKinds
 import scala.util.Try
 import scalaz.MonadPlus
@@ -16,5 +17,18 @@ trait ToMoreMonadPlusOps extends ToMonadPlusOps {
     // because flatMap only works on collections
     def oMap[B](f: A => Option[B]): F[B] = $.map(f).filter(_.isDefined).map(_.get)
     def select[B <: A : Manifest]: F[B] = oMap(_.safeCast[B])
+    /** Not thread-safe! */
+    def uniqueBy[B](f: A => B): F[A] = {
+      val set = new mutable.HashSet[B]()
+      $.filter(a => {
+        val b = f(a)
+        if (set(b))
+          false
+        else {
+          set += b
+          true
+        }
+      })
+    }
   }
 }
