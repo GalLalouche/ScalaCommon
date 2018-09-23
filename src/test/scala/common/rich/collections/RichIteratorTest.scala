c9533b04f9b61b73de71a910392fcd45990a4666
Gal PC <glpkmtg@gmail.com>
2018-09-23 20:15:57 +0300

    RichIterator fold and reduce

--- src/test/scala/common/rich/collections/RichIteratorTest.scala
+++ src/test/scala/common/rich/collections/RichIteratorTest.scala
@@ -14,39 +14,71 @@ import scala.util.Random
 
 class RichIteratorTest extends FreeSpec with AuxSpecs with TimeLimitedTests {
   override val timeLimit = 2 seconds
+  private def compareIterators[T](actual: Iterator[T], expected: TraversableOnce[T]): Unit =
+    actual.toList shouldReturn expected.toList
 
   "verifyForAll" - {
     "throws an exception if f is not satisfied" in {
       an[Exception] should be thrownBy {
-        List(1, 2, 3).iterator.verify(_ < 3).toVector
+        Iterator(1, 2, 3).verify(_ < 3).toVector
       }
     }
 
     "doesn't throw an exception if all passes" in {
-      List(1, 2, 3).iterator.verify(_ < 4).toList shouldReturn List(1, 2, 3)
+      compareIterators(Iterator(1, 2, 3).verify(_ < 4), Iterator(1, 2, 3))
     }
   }
 
   "zipWithIndex zips with index" in {
-    List(1.0, 2.0, 3.0).iterator.zipWithIndex.toList shouldReturn List((1.0, 0), (2.0, 1), (3.0, 2))
+    compareIterators(Iterator(1.0, 2.0, 3.0).zipWithIndex, List((1.0, 0), (2.0, 1), (3.0, 2)))
   }
 
-  "par" - {
+  "reducingIterator" - {
+    "empty" in {
+      Iterator[Int]().reducingIterator((_, _) => ???) shouldBe 'empty
+    }
+    "single element" in {
+      compareIterators(Iterator(1).reducingIterator(_ + _), List(1))
+    }
+    "two elements" in {
+      compareIterators(Iterator(1, 2).reducingIterator(_ + _), List(1, 3))
+    }
+    "five elements" in {
+      compareIterators(Iterator(1, 2, 3, 4, 5).reducingIterator(_ + _), List(1, 3, 6, 10, 15))
+    }
+  }
+  "foldingIterator" - {
+    def fold(i: Int, s: String): Int = i + s.length
+    "empty" in {
+      compareIterators(Iterator[String]().foldingIterator(5)(fold), Seq(5))
+    }
+    "single element" in {
+      compareIterators(Iterator("foo").foldingIterator(5)(fold), List(5, 8))
+    }
+    "two elements" in {
+      compareIterators(Iterator("foobar", "bazz").foldingIterator(5)(fold), List(5, 11, 15))
+    }
+    "five elements" in {
+      compareIterators(Iterator("foo", "bar", "bazz", "quux", "moomoo").foldingIterator(5)(fold), List(5, 8, 11, 15, 19, 25))
+    }
+  }
+
+  "par" ignore {
     "create threads to run a map request" ignore {
       val set = new mutable.HashSet[Object]()
       val r = new Random()
-      List.fill(100)(r.nextDouble).toIterator.par().foreach(_ => set.synchronized {
+      Iterator.fill(100)(r.nextDouble).par().foreach(_ => set.synchronized {
         set.add(Thread.currentThread())
       })
       set.size should be > 1
     }
 
     "returns the items ignore order" ignore {
-      List(1, 2, 3, 4, 5, 6).iterator.par().map(_ * 2).toList shouldReturn List(2, 4, 6, 8, 10, 12)
+      Iterator(1, 2, 3, 4, 5, 6).par().map(_ * 2).toList shouldReturn List(2, 4, 6, 8, 10, 12)
     }
 
     "works on lists of size 10" ignore {
-      (1 to 10).toList.iterator.par().map(_ * 2).toList shouldReturn (2 to 20 by 2).toList
+      (1 to 10).iterator.par().map(_ * 2).toList shouldReturn (2 to 20 by 2).toList
     }
 
     "runs work ignore parallel and be faster than a serialize execution" ignore {
