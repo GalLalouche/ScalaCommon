dc0679f957d637c29e134821f8386d7ed2a2ac85
Gal PC <glpkmtg@gmail.com>
2018-10-08 21:03:37 +0300

    ToMoreBindOps.ifFalse/ifTrue

--- src/main/scala/common/rich/func/ToMoreBindOps.scala
+++ src/main/scala/common/rich/func/ToMoreBindOps.scala
@@ -6,7 +6,11 @@ import scalaz.{Bind, OptionT}
 import scalaz.syntax.ToBindOps
 
 trait ToMoreBindOps extends ToBindOps {
+  import scalaz.Leibniz.===
+
   implicit class toMoreBindOps[F[_] : Bind, A]($: F[A]) {
     def toOptionTB[B](f: A => F[Option[B]]): OptionT[F, B] = OptionT($.flatMap(f))
+    def ifFalse(f: F[Unit])(implicit ev: A === Boolean): F[A] = $.ifM(ifTrue = $, ifFalse = f >> $)
+    def ifTrue(f: F[Unit])(implicit ev: A === Boolean): F[A] = $.ifM(ifTrue = f >> $, ifFalse = $)
   }
 }
