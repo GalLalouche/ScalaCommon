390d387f5c3e5cc9696bf122db9673264180561a
Gal PC <glpkmtg@gmail.com>
2018-10-11 21:18:54 +0300

    RichTuple2 stuff

--- src/main/scala/common/rich/RichTuple.scala
+++ src/main/scala/common/rich/RichTuple.scala
@@ -2,13 +2,18 @@ package common.rich
 
 /** Mostly for adding maps and stuff. */
 object RichTuple {
-  implicit class richTuple[T](private val $: (T, T)) extends AnyVal {
-    def map[S](f: T => S): (S, S) = (f($._1), f($._2))
-    def foreach(f: T => Unit) {f($._1); f($._2)}
+  implicit class richTuple2[A, B](private val $: (A, B)) extends AnyVal {
+    def reduce[C](f: (A, B) => C): C = f($._1, $._2)
+    def modifySecond[C](f: B => C): (A, C) = $._1 -> f($._2)
+    def modifyFirst[C](f: A => C): (C, B) = f($._1) -> $._2
+  }
+  implicit class richSameTuple2[A](private val $: (A, A)) extends AnyVal {
+    def map[S](f: A => S): (S, S) = (f($._1), f($._2))
+    def foreach(f: A => Unit) {f($._1); f($._2)}
     def toList = List($._1, $._2)
   }
 
-  implicit class richTupleSeqs[T, S](private val $: (Seq[T], Seq[S])) extends AnyVal {
-    def zip: Seq[(T, S)] = $._1.zip($._2)
+  implicit class richTupleSeqs[A, B](private val $: (Seq[A], Seq[B])) extends AnyVal {
+    def zip: Seq[(A, B)] = $._1.zip($._2)
   }
 }
