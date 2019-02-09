e12b33cd58602897a41be5e0b0747f622e1c3759
Gal PC <glpkmtg@gmmail.com>
2019-02-09 13:37:04 +0200

    Cleanup TableUtilsTemplate

--- src/main/scala/common/storage/TableUtilsTemplate.scala
+++ src/main/scala/common/storage/TableUtilsTemplate.scala
@@ -6,20 +6,12 @@ import scala.concurrent.{ExecutionContext, Future}
 import scalaz.std.FutureInstances
 import scalaz.syntax.{ToBindOps, ToFunctorOps}
 
-/** Provides overrides since the TableUtils trait can't have implicit parameters. */
+/** Provides overrides requiring an ExecutionContext since the TableUtils trait can't have implicit parameters. */
 abstract class TableUtilsTemplate(implicit ec: ExecutionContext) extends TableUtils
     with FutureInstances with ToFunctorOps with ToBindOps {
-  /** Fails if the table already exists. */
-  def createTable(): Future[_]
-  /** Fails if the table doesn't exist. */
-  def clearTable(): Future[_]
-  /** If the table doesn't exit, create it; otherwise, clear it. */
-  def clearOrCreateTable(): Future[ClearOrCreateResult] =
+  override def clearOrCreateTable(): Future[ClearOrCreateResult] =
     doesTableExist.ifM(clearTable() >| Cleared, createTable() >| Created)
-
   protected def forceDropTable(): Future[_]
-  /** Returns true if the table existed before and was actually dropped. */
-  def dropTable(): Future[Boolean] =
+  override def dropTable(): Future[Boolean] =
     doesTableExist.ifM(forceDropTable() >| true, Future successful false)
-  def doesTableExist: Future[Boolean]
 }
