e5524513500dae62aed49dbcedbf051c5b07f096
Gal PC <glpkmtg@gmmail.com>
2019-02-09 14:00:41 +0200

    RichSet.concurrentSet

--- src/main/scala/common/rich/collections/RichSet.scala
+++ src/main/scala/common/rich/collections/RichSet.scala
@@ -1,6 +1,12 @@
 package common.rich.collections
 
+import java.util.Collections
+import java.util.concurrent.ConcurrentHashMap
+
 import common.rich.primitives.RichBoolean._
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 object RichSet {
   implicit class richSet[T](private val $: Set[T]) extends AnyVal {
@@ -10,4 +16,7 @@ object RichSet {
     def >(other: Set[T]): Boolean = new richSet(other) < $
     def isDisjointTo(other: Set[T]): Boolean = $.exists(other.contains).isFalse
   }
+
+  def concurrentSet[A]: mutable.Set[A] =
+    Collections.newSetFromMap(new ConcurrentHashMap[A, java.lang.Boolean]).asScala
 }
