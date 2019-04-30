0e7b81b68b76cb241c5718dd38b89ffc5e8c5678
Gal Lalouche <gal.lalouche@oath.com>
2019-04-30 15:39:39 +0300

    Percentage

---
+++ src/main/scala/common/Percentage.scala
@@ -0,0 +1,47 @@
+package common
+
+import common.rich.primitives.RichDouble._
+
+import scala.language.implicitConversions
+import scala.util.Random
+
+class Percentage private(val p: Double) extends AnyVal {
+  def >=(d: Double): Boolean = p >= d
+  def <=(d: Double): Boolean = p <= d
+
+  def *(e: Int): Int = math.round(p * e).toInt
+  def *(e: Long): Long = math.round(p * e)
+  def *(e: Double): Double = p * e
+  def *(e: Float): Double = p * e
+  override def toString: String = p withPrecision 2
+  /** Returns true with probability p. */
+  def roll(r: Random): Boolean = r.nextDouble() <= p
+}
+
+object Percentage {
+  def conform(p: Double): Percentage =
+    if (p <= 0) Percentage(0.0) else if (p >= 1) Percentage(1.0) else p
+
+  // One might wonder what's the point of an implicit def in this case, since it forgoes all type
+  // safety. The answer is laziness (of course), and Percentage still enjoys the added benefit of
+  // bound checking at runtime, as well as revealing intent to clients.
+  implicit def toPercentage(d: Double): Percentage = Percentage(d)
+  def apply(x: Double, y: Double): Percentage = apply(x / y)
+  def apply(p: Double): Percentage = {
+    require(p >= 0 && p <= 1)
+    new Percentage(p)
+  }
+
+  implicit val orderingEv: Ordering[Percentage] = Ordering.by(_.p)
+
+  /**
+   * Equivalent to {{{0.0 to 1.0 by step}}}. This method is needed since it's possible to get
+   * numbers which are larger than 1.0 due to floating point errors. The last number in returned
+   * sequence is guaranteed to always be 1.0.
+   */
+  def step(by: Percentage): Seq[Percentage] = {
+    val p = by.p
+    require(p > 0 && p < 0.5)
+    (0.0 to 1.0 by p).map(Math.min(1, _)).map(Percentage.apply)
+  }
+}
---
+++ src/test/scala/common/PercentageTest.scala
@@ -0,0 +1,46 @@
+package common
+
+import org.scalatest.FreeSpec
+
+import scala.util.Random
+
+class PercentageTest extends FreeSpec with AuxSpecs {
+  "constructor throws on values out of bounds" in {
+    an[IllegalArgumentException] shouldBe thrownBy {Percentage(-1e-5)}
+    an[IllegalArgumentException] shouldBe thrownBy {Percentage(1 + 1e-5)}
+  }
+  "conform" in {
+    Percentage.conform(-1e-5).p shouldReturn 0
+    Percentage.conform(1 + 1e-5).p shouldReturn 1
+  }
+  "apply ratio" in {
+    Percentage(1, 2).p shouldReturn 0.5
+  }
+  "*" in {
+    Percentage(0.23) * 100 shouldReturn 23
+    Percentage(0.23) * 100L shouldReturn 23L
+    Percentage(0.25) * 0.75 shouldReturn 0.25 * 0.75
+    Percentage(0.25) * 0.75f shouldReturn 0.25 * 0.75
+  }
+  "step" in {
+    Percentage.step(0.1).map(_ * 10) shouldReturn 0.to(10)
+  }
+  "comparisons" - {
+    "ordering" in {
+      Vector[Percentage](0.5, 0.25, 0.75).sorted shouldReturn Vector(0.25, 0.5, 0.75)
+    }
+    ">=" in {
+      Percentage(0.5) >= 0.25 shouldReturn true
+      Percentage(0.5) >= 0.75 shouldReturn false
+    }
+    "<=" in {
+      Percentage(0.5) <= 0.25 shouldReturn false
+      Percentage(0.5) <= 0.75 shouldReturn true
+    }
+  }
+
+  "random" in {
+    Percentage(1.0).roll(new Random(0)) shouldReturn true
+    Percentage(0.0).roll(new Random(0)) shouldReturn false
+  }
+}
