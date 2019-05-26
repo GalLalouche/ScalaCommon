633c01306f9fac7fa9d2a3b3a4d6489f1114a830
Gal Lalouche <gal.lalouche@oath.com>
2019-05-26 15:09:56 +0300

    ToMoreFunctorOps.when
    Also test other methods

--- src/main/scala/common/rich/func/ToMoreFunctorOps.scala
+++ src/main/scala/common/rich/func/ToMoreFunctorOps.scala
@@ -1,16 +1,19 @@
 package common.rich.func
 
+import common.rich.RichT._
+import scalaz.syntax.ToFunctorOps
+import scalaz.{Functor, OptionT}
+
 import scala.language.higherKinds
 
-import scalaz.{Functor, OptionT}
-import scalaz.syntax.ToFunctorOps
-
 trait ToMoreFunctorOps extends ToFunctorOps {
-  implicit class toMoreFunctorOps[F[_]: Functor, A]($: F[A]) {
-    def listen(f: A => Any): F[A] = $.map(e => {f(e); e})
+  implicit class toMoreFunctorOps[F[_] : Functor, A]($: F[A]) {
+    def listen(f: A => Any): F[A] = $.map(_ <| f)
     def toOptionTF[B](f: A => Option[B]): OptionT[F, B] = OptionT($.map(f))
     def unzip[B, C](implicit ev: A <:< (B, C)): (F[B], F[C]) = $.map(_._1) -> $.map(_._2)
     def ifNone[B](default: => B)(implicit ev: A <:< Option[B]): F[B] = $.map(_ getOrElse default)
+    def when[B](whenTrue: B, whenFalse: B)(implicit ev: A =:= Boolean): F[B] =
+      $.map(b => if (ev(b)) whenTrue else whenFalse)
   }
 }
 
---
+++ src/test/scala/common/rich/func/ToMoreFunctorOpsTest.scala
@@ -0,0 +1,33 @@
+package common.rich.func
+
+import common.AuxSpecs
+import common.rich.RichFuture._
+import common.rich.RichObservable._
+import org.scalatest.FreeSpec
+import rx.lang.scala.Observable
+import scalaz.std.VectorInstances
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ToMoreFunctorOpsTest extends FreeSpec with AuxSpecs
+    with ToMoreFunctorOps with MoreObservableInstances with VectorInstances {
+  "listen" in {
+    var sum = 0
+    val $: Observable[Int] = Observable.just(1, 2, 3, 4).listen(sum += _)
+    var product = 1
+    $.foreach(product *= _)
+    sum shouldReturn 10
+    product shouldReturn 24
+  }
+  "unzip" in {
+    val (o1, o2): (Observable[Int], Observable[Int]) = Observable.just(1 -> 2, 3 -> 4).unzip
+    o1.toFuture[Vector].get shouldReturn Vector(1, 3)
+    o2.toFuture[Vector].get shouldReturn Vector(2, 4)
+  }
+  "ifNone" in {
+    Vector(None, Some(3), None).ifNone(42) shouldReturn Vector(42, 3, 42)
+  }
+  "when" in {
+    Vector(true, false, true).when(whenTrue = 1, whenFalse = 2) shouldReturn Vector(1, 2, 1)
+  }
+}
