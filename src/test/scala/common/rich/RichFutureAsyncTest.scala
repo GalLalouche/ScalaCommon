b1ee05b9f1da0b698019d52cff5a893b8376f3b0
Gal Lalouche <gal.lalouche@oath.com>
2019-12-11 14:22:30 -0800

    RichFutureTest test split + remove time limit

---
+++ src/test/scala/common/rich/RichFutureAsyncTest.scala
@@ -0,0 +1,53 @@
+package common.rich
+
+import org.scalatest.{AsyncFreeSpec, OneInstancePerTest}
+
+import scala.concurrent.Future
+
+import scalaz.std.scalaFuture.futureInstance
+import scalaz.syntax.functor.ToFunctorOps
+
+import common.AuxSpecs
+import common.rich.RichFuture._
+
+class RichFutureAsyncTest extends AsyncFreeSpec with AuxSpecs with OneInstancePerTest {
+  private def success: Future[Int] = Future successful 1
+  private val exception = new Exception("Derp")
+  private def failure: Future[Int] = Future failed exception
+  private def invoked(f: (=> Any) => Future[_]) = {
+    var x = 0
+    f {x += 1}.toTry.>|(x shouldReturn 1)
+  }
+  private def notInvoked(f: (=> Any) => Future[_]) = {
+    var x = 0
+    f {x += 1}.toTry.>|(x shouldReturn 0)
+  }
+  "RichFuture" - {
+    "|<" - {
+      "success" in invoked(success.|<)
+      "failure" in invoked(failure.|<)
+    }
+    "onSuccessful" - {
+      "success" in invoked(success.onSuccessful)
+      "failure" in notInvoked(failure.onSuccessful)
+    }
+    "onFailed" - {
+      "success" in notInvoked(success.onFailed)
+      "failure" in invoked(failure.onFailed)
+    }
+    "consumeTry" - {
+      "success" in {
+        var succeeded = false
+        success.consumeTry(e => succeeded = e.isSuccess)
+            .map(_.shouldReturn(1) && succeeded.shouldReturn(true))
+      }
+      "failure" in {
+        var succeeded = true
+        failure
+            .consumeTry(e => succeeded = e.isSuccess)
+            .toTry
+            .map(_ => succeeded.shouldReturn(false))
+      }
+    }
+  }
+}
--- src/test/scala/common/rich/RichFutureTest.scala
+++ src/test/scala/common/rich/RichFutureTest.scala
@@ -1,86 +1,44 @@
 package common.rich
 
-import org.scalatest.{FreeSpec, OneInstancePerTest}
+import java.util.concurrent.Executors
 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
+import org.scalatest.FreeSpec
+
+import scala.concurrent.{ExecutionContext, Future}
 
 import common.AuxSpecs
 import common.rich.RichFuture._
 
-class RichFutureTest extends FreeSpec with AuxSpecs with OneInstancePerTest {
-  private val success: Future[Int] = Future successful 1
+class RichFutureTest extends FreeSpec with AuxSpecs {
+  private implicit val ec: ExecutionContext =
+    ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
+  private def success: Future[Int] = Future {
+    Thread sleep 10
+    5
+  }
   private val exception = new Exception("Derp")
-  private val failure: Future[Int] = Future failed exception
-  def shouldFail(f: Future[Any]) = {
-    Await.ready(f, Duration.Inf)
-    val t = f.value.get
-    t.isFailure shouldReturn true
+  private def failure: Future[Int] = Future {
+    Thread sleep 10
+    throw exception
   }
-  def invoked(f: (=> Any) => Future[_]): Unit = {
-    var x = 0
-    f {x += 1}.toTry.get
-    x shouldReturn 1
+
+  "get" - {
+    "success" in {
+      success.get shouldReturn 5
+    }
+    "failure" in {
+      an[Exception] should be thrownBy failure.get
+    }
   }
-  def notInvoked(f: (=> Any) => Future[_]): Unit = {
-    var x = 0
-    f {x += 1}.toTry.get
-    x shouldReturn 0
-  }
-  "RichFuture" - {
-    "|<" - {
-      "success" in invoked(success.|<)
-      "failure" in invoked(failure.|<)
+
+  "getFailure" - {
+    "when success should throw" in {
+      an[UnsupportedOperationException] should be thrownBy success.getFailure
     }
-    "onSuccessful" - {
-      "success" in invoked(success.onSuccessful)
-      "failure" in notInvoked(failure.onSuccessful)
-    }
-    "onFailed" - {
-      "success" in notInvoked(success.onFailed)
-      "failure" in invoked(failure.onFailed)
-    }
-    "get" - {
-      "success" in {
-        val f = Future {
-          Thread.sleep(10)
-          5
-        }
-        f.get shouldReturn 5
-      }
-      "failure" in {
-        val f = Future {
-          Thread.sleep(10)
-          throw new Exception()
-        }
-        an[Exception] should be thrownBy f.get
-      }
-    }
-    "getFailure" - {
-      "when success should throw" in {
-        val f = Future[Int](1)
-        an[UnsupportedOperationException] should be thrownBy f.getFailure
-      }
-      "when failure should return the error" in {
-        val f = failure
-        val e = f.getFailure
-        e shouldReturn exception
-      }
-    }
-    "consumeTry" - {
-      "success" in {
-        var succeeded = false
-        val value = success.consumeTry(e => succeeded = e.isSuccess).get
-        value shouldReturn 1
-        succeeded shouldReturn true
-      }
-      "failure" in {
-        var succeeded = true
-        val value = failure.consumeTry(e => succeeded = e.isSuccess).getFailure
-        value shouldReturn exception
-        succeeded shouldReturn false
-      }
+    "when failure should return the error" in {
+      val f = failure
+      val e = f.getFailure
+      e shouldReturn exception
     }
   }
 }
