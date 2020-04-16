c56f61ada54e116614896f6c7679c7d3151ef8e0
Gal PC <glpkmtg@gmail.com>
2020-04-16 18:19:51 +0300

    Use RichOptionT in RichStreamTTest

--- src/test/scala/common/rich/func/RichStreamTTest.scala
+++ src/test/scala/common/rich/func/RichStreamTTest.scala
@@ -4,7 +4,6 @@ import org.scalatest.AsyncFreeSpec
 
 import scala.concurrent.Future
 
-import scalaz.OptionT
 import scalaz.std.scalaFuture.futureInstance
 
 import common.test.AsyncAuxSpecs
@@ -12,15 +11,12 @@ import common.test.AsyncAuxSpecs
 class RichStreamTTest extends AsyncFreeSpec with AsyncAuxSpecs {
   "iterateM" - {
     "basic" in {
-      RichStreamT.iterateM[Int, Future](1)(
-        e => if (e < 5) OptionT(Future.successful(Option(e + 1))) else OptionT.none
-      ).toStream
+      RichStreamT.iterateM(1)(e => RichOptionT.when(e < 5)(Future successful e + 1))
+          .toStream
           .map(_ shouldReturn 1.to(5).toStream)
     }
     "infinite" in {
-      RichStreamT.iterateM[Int, Future](1)(
-        e => OptionT(Future.successful(Option(e + 1)))
-      ).take(5)
+      RichStreamT.iterateM(1)(e => RichOptionT.pointSome[Future].apply(e + 1)).take(5)
           .toStream
           .map(_ shouldReturn 1.to(5).toStream)
     }
