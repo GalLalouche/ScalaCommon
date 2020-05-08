507a2513e916a69560c5fa9d3e57ae9c0834f8f2
Gal PC <glpkmtg@gmail.com>
2020-05-08 11:37:55 +0300

    Remove needless RichTuple functions
    Tuple is a also a BiFunctor, among other things, so it's not
    necessary to provide those things specfically for tuples.

--- src/main/scala/common/rich/RichTuple.scala
+++ src/main/scala/common/rich/RichTuple.scala
@@ -2,19 +2,12 @@ package common.rich
 
 /** Mostly for adding maps and stuff. */
 object RichTuple {
+  implicit class richSameTuple2[A](private val $: (A, A)) extends AnyVal {
+    def map[B](f: A => B): (B, B) = f($._1) -> f($._2)
+  }
   implicit class richTuple2[A, B](private val $: (A, B)) extends AnyVal {
     def reduce[C](f: (A, B) => C): C = f($._1, $._2)
     def modifySecond[C](f: B => C): (A, C) = $._1 -> f($._2)
     def modifyFirst[C](f: A => C): (C, B) = f($._1) -> $._2
-    def map2[C, D](f: A => C, g: B => D): (C, D) = f($._1) -> g($._2)
-  }
-  implicit class richSameTuple2[A](private val $: (A, A)) extends AnyVal {
-    def map[S](f: A => S): (S, S) = (f($._1), f($._2))
-    def foreach(f: A => Unit) {f($._1); f($._2)}
-    def toList = List($._1, $._2)
-  }
-
-  implicit class richTupleSeqs[A, B](private val $: (Seq[A], Seq[B])) extends AnyVal {
-    def zip: Seq[(A, B)] = $._1.zip($._2)
   }
 }
