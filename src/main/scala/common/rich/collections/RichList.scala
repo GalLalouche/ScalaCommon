e3574c17373e36e2f5b394fe25d7c8f46134831f
Gal Lalouche <gal.lalouche@oath.com>
2020-05-24 10:27:15 +0300

    RichSeq: remove appending and prepending methods

--- src/main/scala/common/rich/collections/RichList.scala
+++ src/main/scala/common/rich/collections/RichList.scala
@@ -4,7 +4,7 @@ object RichList {
   implicit class richList[A](private val $: List[A]) extends AnyVal {
     def headTailOption: Option[(A, List[A])] = $ match {
       case Nil => None
-      case ::(head, tail) => Some(head -> tail)
+      case head :: tail => Some(head -> tail)
     }
   }
 }
