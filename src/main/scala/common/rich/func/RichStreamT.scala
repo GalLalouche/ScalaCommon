7ae1eee8a290c4d0371ff06846385a02990f67c0
Gal PC <glpkmtg@gmmail.com>
2020-07-10 18:01:43 +0300

    RichStreamT.fromEvaluatedIterable

--- src/main/scala/common/rich/func/RichStreamT.scala
+++ src/main/scala/common/rich/func/RichStreamT.scala
@@ -2,8 +2,9 @@ package common.rich.func
 
 import scala.language.higherKinds
 
-import scalaz.{Applicative, Monad, OptionT, StreamT}
+import scalaz.{~>, Applicative, Hoist, Monad, OptionT, StreamT}
 import scalaz.syntax.functor.ToFunctorOps
+import scalaz.Id.Id
 
 object RichStreamT {
   implicit class richStreamT[A, F[_]](private val $: StreamT[F, A]) extends AnyVal {
@@ -17,4 +18,8 @@ object RichStreamT {
   }
   def singleton[F[_] : Applicative, A](value: F[A]): StreamT[F, A] =
     StreamT.fromStream(value.map(Stream(_)))
+  def fromEvaluatedIterable[F[_] : Point, A](i: Iterable[A]): StreamT[F, A] = Hoist[StreamT].hoist(
+    new (Id ~> F) {
+      override def apply[X](fa: scalaz.Id.Id[X]) = Point[F].point(fa)
+    }).apply(StreamT.fromIterable(i))
 }
--- src/test/scala/common/rich/func/RichStreamTTest.scala
+++ src/test/scala/common/rich/func/RichStreamTTest.scala
@@ -9,16 +9,30 @@ import common.rich.func.BetterFutureInstances._
 import common.test.AsyncAuxSpecs
 
 class RichStreamTTest extends AsyncFreeSpec with AsyncAuxSpecs {
-  "iterateM" - {
-    "basic" in {
-      RichStreamT.iterateM(1)(e => RichOptionT.when(e < 5)(Future successful e + 1))
-          .toStream
-          .map(_ shouldReturn 1.to(5).toStream)
+  "Object methods" - {
+    "iterateM" - {
+      "basic" in {
+        RichStreamT.iterateM(1)(e => RichOptionT.when(e < 5)(Future successful e + 1))
+            .toStream
+            .map(_ shouldReturn 1.to(5).toStream)
+      }
+      "infinite" in {
+        RichStreamT.iterateM(1)(e => RichOptionT.pointSome[Future].apply(e + 1)).take(5)
+            .toStream
+            .map(_ shouldReturn 1.to(5).toStream)
+      }
     }
-    "infinite" in {
-      RichStreamT.iterateM(1)(e => RichOptionT.pointSome[Future].apply(e + 1)).take(5)
-          .toStream
-          .map(_ shouldReturn 1.to(5).toStream)
+
+    "fromEvaluatedIterable" - {
+      "empty" in {
+        RichStreamT.fromEvaluatedIterable[Future, Int](Nil).mapValue(_ shouldReturn Stream.empty)
+      }
+      "single element" in {
+        RichStreamT.fromEvaluatedIterable[Future, Int](Vector(1)).mapValue(_ shouldReturn Stream(1))
+      }
+      "multiple element" in {
+        RichStreamT.fromEvaluatedIterable[Future, Int](List(1, 2)).mapValue(_ shouldReturn Stream(1, 2))
+      }
     }
   }
 }
