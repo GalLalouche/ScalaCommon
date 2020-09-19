61241813aee471aaa49ade4bdc557ff7e5421953
Gal PC <glpkmtg@gmmail.com>
2020-09-19 12:02:12 +0300

    RichTime

---
+++ src/main/scala/common/rich/RichTime.scala
@@ -0,0 +1,30 @@
+package common.rich
+
+import java.time._
+
+import scala.math.Ordered.orderingToOrdered
+
+import common.rich.primitives.RichBoolean.richBoolean
+
+object RichTime {
+  implicit val OrderingLocalDateTime: Ordering[LocalDateTime] = Ordering.by(_.toMillis)
+  implicit val OrderingLocalDate: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
+
+  implicit class RichInstant($: Instant) {
+    def toLocalDateTime: LocalDateTime = LocalDateTime from $.atZone(ZoneId.systemDefault)
+    def toLocalDate: LocalDate = LocalDate from $.atZone(ZoneId.systemDefault)
+  }
+  implicit class RichLong($: Long) {
+    def toLocalDateTime: LocalDateTime = Instant.ofEpochMilli($).toLocalDateTime
+  }
+  implicit class RichClock($: Clock) {
+    def getLocalDateTime: LocalDateTime = $.instant.atZone(ZoneId.systemDefault).toLocalDateTime
+    def getLocalDate: LocalDate = $.instant.toLocalDate
+  }
+  implicit class RichLocalDateTime($: LocalDateTime) {
+    def toMillis: Long = $.atZone(ZoneId.systemDefault).toInstant.toEpochMilli
+    def age(c: Clock): Duration = Duration.between($, c.getLocalDateTime)
+    def isNewerThan(d: Duration, c: Clock): Boolean = age(c) < d
+    def isOlderThan(d: Duration, c: Clock): Boolean = isNewerThan(d, c).isFalse
+  }
+}
---
+++ src/test/scala/common/rich/RichTimeTest.scala
@@ -0,0 +1,24 @@
+package common.rich
+
+import java.time.{Clock, Duration, Instant, LocalDateTime, ZoneId}
+
+import org.scalatest.FreeSpec
+
+import common.rich.RichTime._
+import common.test.AuxSpecs
+
+class RichTimeTest extends FreeSpec with AuxSpecs {
+  private val clock = Clock.fixed(Instant.ofEpochSecond(2000, 1000), ZoneId.systemDefault())
+  "RichLocalDateTime" - {
+    "age" in {
+      val age = LocalDateTime.ofInstant(Instant.ofEpochSecond(1000, 500), ZoneId.systemDefault())
+          .age(clock)
+      age.getSeconds shouldReturn 1000
+      age.getNano shouldReturn 500
+    }
+    "isNewerThan" in {
+      val t1 = LocalDateTime.ofInstant(Instant.ofEpochSecond(1000, 500), ZoneId.systemDefault())
+      t1.isNewerThan(Duration.ofMillis(1000), clock) shouldReturn false
+    }
+  }
+}
