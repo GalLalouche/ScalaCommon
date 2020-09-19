36219f96182aa8cc4c8a6c6fbab50baf61bc978c
Gal PC <glpkmtg@gmmail.com>
2020-09-19 11:43:00 +0300

    NumericMonoids

---
+++ src/main/scala/common/rich/func/NumericMonoids.scala
@@ -0,0 +1,14 @@
+package common.rich.func
+
+import scalaz.Monoid
+
+object NumericMonoids {
+  implicit def SumNumeric[N](implicit ev: Numeric[N]): Monoid[N] = new Monoid[N] {
+    override def zero = ev.fromInt(0)
+    override def append(f1: N, f2: => N) = ev.plus(f1, f2)
+  }
+  implicit def ProductNumeric[N](implicit ev: Numeric[N]): Monoid[N] = new Monoid[N] {
+    override def zero = ev.fromInt(1)
+    override def append(f1: N, f2: => N) = ev.times(f1, f2)
+  }
+}
