61c0cf26caf69a2b08a3efecea9b57726bb5f1ba
Gal PC <glpkmtg@gmmail.com>
2021-02-12 20:31:05 +0200

    RichStream.compute

--- src/main/scala/common/rich/collections/RichStream.scala
+++ src/main/scala/common/rich/collections/RichStream.scala
@@ -1,9 +1,15 @@
 package common.rich.collections
 
+import scala.annotation.tailrec
+
 import common.rich.RichT._
 
 object RichStream {
   implicit class richStream[A](private val $: Stream[A]) extends AnyVal {
     def tailOption: Option[Stream[A]] = $.tail.optFilter(_.nonEmpty)
+    def compute(n: Int): Unit = {
+      @tailrec def go(s: Stream[_], n: Int): Unit = if (n <= 0) () else go(s.tail, n - 1)
+      go($, n - 1)
+    }
   }
 }
--- src/test/scala/common/rich/collections/RichStreamTest.scala
+++ src/test/scala/common/rich/collections/RichStreamTest.scala
@@ -2,6 +2,9 @@ package common.rich.collections
 
 import org.scalatest.FreeSpec
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 import common.rich.collections.RichStream._
 import common.test.AuxSpecs
 
@@ -15,6 +18,42 @@ class RichStreamTest extends FreeSpec with AuxSpecs {
     }
     "Multiple elements returns Some" in {
       Stream(1, 2, 3).tailOption shouldReturn Some(Stream(2, 3))
+    }
+  }
+  "compute" - {
+    // The returned buffer will only be changed on computations other than the head.
+    def go: (mutable.Buffer[Int], Stream[_]) = {
+      var x = -1
+      val buffer = new ArrayBuffer[Int]()
+      val stream = Stream.continually {
+        x +=1
+        if (x > 0)
+          buffer += x
+        Unit
+      }
+      (buffer, stream)
+    }
+    "0 does nothing" in {
+      val (buffer, stream) = go
+      stream.compute(0)
+      buffer shouldBe 'empty
+    }
+    "1 does nothing, because head is already strict" in {
+      val (buffer, stream) = go
+      stream.compute(1)
+      buffer shouldBe 'empty
+    }
+    "5 only computes 4 times" in {
+      val (buffer, stream) = go
+      stream.compute(5)
+      buffer shouldReturn 1.to(4).toBuffer
+    }
+    "Does not compute extra times" in {
+      val (buffer, stream) = go
+      stream.compute(5)
+      buffer.clear()
+      stream.compute(5)
+      buffer shouldBe 'empty
     }
   }
 }
