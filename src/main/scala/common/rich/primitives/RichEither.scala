d45cad0ef9b79521d9e832ce67420ca7fb614feb
Gal PC <glpkmtg@gmmail.com>
2022-03-05 11:45:23 +0200

    RichEither.strengthens

---
+++ src/main/scala/common/rich/primitives/RichEither.scala
@@ -0,0 +1,11 @@
+package common.rich.primitives
+
+import scalaz.std.either.eitherInstance
+import scalaz.syntax.bifunctor.ToBifunctorOps
+
+object RichEither {
+  implicit class richEither[A, B](private val $: Either[A, B]) extends AnyVal {
+    def eitherStrengthenLeft[C](c: C): Either[(A, C), (B, C)] = $.bimap(_ -> c, _ -> c)
+    def eitherStrengthenRight[C](c: C): Either[(C, A), (C, B)] = $.bimap(c -> _, c -> _)
+  }
+}
