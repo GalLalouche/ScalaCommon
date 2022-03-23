2f2c0bc433a4afbedf14779d72c13d94c82f4572
Gal (Windows) <>
2022-03-23 15:25:29 +0200

    BeforeAndAfterAllAsync, fail on setup if before or after fails

--- src/main/scala/common/test/BeforeAndAfterAllAsync.scala
+++ src/main/scala/common/test/BeforeAndAfterAllAsync.scala
@@ -9,9 +9,9 @@ trait BeforeAndAfterAllAsync extends AsyncTestSuiteMixin {self: AsyncTestSuite =
   protected def beforeAll(): Future[_] = BeforeAndAfterAllAsync.EmptyFuture
   protected def afterAll(): Future[_] = BeforeAndAfterAllAsync.EmptyFuture
   abstract override def run(testName: Option[String], args: Args): Status = {
-    Await.ready(beforeAll(), Duration.Inf)
+    Await.result(beforeAll(), Duration.Inf)
     val $ = super.run(testName, args)
-    Await.ready(afterAll(), Duration.Inf)
+    Await.result(afterAll(), Duration.Inf)
     $
   }
 }
