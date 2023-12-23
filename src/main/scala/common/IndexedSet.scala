5e27212d3fc08c9cd0e0d4c13ad021e95b34a39d
Gal (Windows) <>
2023-12-23 23:49:49 +0200

    IndexedSet

---
+++ src/main/scala/common/IndexedSet.scala
@@ -0,0 +1,24 @@
+package common
+
+import scalaz.Semigroup
+
+import common.rich.collections.RichMap._
+
+/**
+ * Sums values in the same equivalence relation, i.e., are considered the "same" for the purpose of
+ * a set.
+ */
+sealed trait IndexedSet[A] extends Iterable[A] {
+  def +(v: A): IndexedSet[A]
+  def ++(vs: TraversableOnce[A]): IndexedSet[A] = vs.foldLeft(this)(_ + _)
+}
+
+object IndexedSet {
+  private class IndexedSetImpl[V: Semigroup, Key](map: Map[Key, V], index: V => Key)
+      extends IndexedSet[V] {
+    def +(v: V): IndexedSet[V] = new IndexedSetImpl(map.upsert(index(v), v), index)
+    override def iterator = map.valuesIterator
+  }
+
+  def apply[K, V: Semigroup](index: V => K): IndexedSet[V] = new IndexedSetImpl(Map(), index)
+}
---
+++ src/test/scala/common/IndexedSetTest.scala
@@ -0,0 +1,21 @@
+package common
+
+import org.scalatest.FreeSpec
+
+import scalaz.Semigroup
+
+import common.test.AuxSpecs
+
+class IndexedSetTest extends FreeSpec with AuxSpecs {
+  "just work" in {
+    case class Person(name: String, money: Int)
+    implicit object PersonSemigroup extends Semigroup[Person] {
+      override def append(f1: Person, f2: => Person): Person = f1.copy(money = f1.money + f2.money)
+    }
+    IndexedSet[String, Person](_.name) + Person("A", 10) + Person("B", 20) + Person(
+      "A",
+      5,
+    ) shouldMultiSetEqual
+      Vector(Person("A", 15), Person("B", 20))
+  }
+}
