23098559dcb14e65c318b05f7757468d935a0525
Gal (Windows) <>
2025-09-29 01:11:07 +0300

    RichEitherT (for 2.12)

---
+++ src/main/scala-2.12/common/rich/func/RichEitherT.scala
@@ -0,0 +1,13 @@
+package common.rich.func
+
+import scalaz.{\/-, EitherT, Monad}
+
+object RichEitherT {
+  implicit class richEitherT[F[_], E, A](private val $ : EitherT[F, E, A]) extends AnyVal {
+    // This already exists in 7.3x, hence why it's only in the 2.12 src code, so this is just copy-pasted from
+    // that implementation.
+    /** Map on the right of this disjunction. */
+    def mapF[B](f: A => F[B])(implicit M: Monad[F]): EitherT[F, E, B] =
+      $.flatMapF(f.andThen(M.map(_)(\/-.apply)))
+  }
+}
