5957c106cfabab7c2b2a9d3de42e092a6225a535
Gal (Windows) <>
2026-01-02 14:42:42 +0200

    RichInputStream.readLine, closings, and documentation

--- src/main/scala/common/rich/RichInputStream.scala
+++ src/main/scala/common/rich/RichInputStream.scala
@@ -1,6 +1,7 @@
 package common.rich
 
 import java.io.{ByteArrayOutputStream, File, FileOutputStream, InputStream, OutputStream}
+import java.nio.charset.StandardCharsets
 
 import scala.annotation.tailrec
 import scala.io.Source
@@ -9,10 +10,46 @@ import common.rich.RichT._
 
 object RichInputStream {
   implicit class richInputStream(private val $ : InputStream) extends AnyVal {
-    def asString: String = Source.fromInputStream($).mkString
-    def toBytes: Array[Byte] = new ByteArrayOutputStream().<|(writeTo).toByteArray
-    def writeTo(outputStream: OutputStream): Unit = {
-      val buffer = new Array[Byte](16384)
+    /**
+     * Reads a line from the input stream (not including the line endings), or [[None]] if EOF is
+     * reached. Does '''not''' consume the remainder of the stream. Will return `Some("")` for empty
+     * lines (e.g., `"foo\n\n"` contains two lines, the second being empty).
+     */
+    def readLine(): Option[String] = {
+      // 🤮🤮🤮🤮🤮
+      // Can't use BufferedReader or Source since they would consume more of the stream than just the line.
+      val baos = new ByteArrayOutputStream(1 << 7)
+      def readUntilNewLine(): Boolean = {
+        var b = 0
+        var read = false
+        b = $.read()
+        while (b != -1) {
+          read = true
+          if (b == '\n')
+            return true
+          if (b == '\r') {
+            $.read().ensuring(_ == '\n', "Expected \\n after \\r")
+            return true
+          }
+          baos.write(b)
+          b = $.read()
+        }
+        read
+      }
+      if (readUntilNewLine()) Some(baos.toString(StandardCharsets.UTF_8)) else None
+    }
+    /** Closes the input stream. */
+    def asString(): String = {
+      // Not using Using for source backwards compatibility with Scala 2.12
+      val source = Source.fromInputStream($)
+      try source.mkString
+      finally source.close()
+    }
+    /** Closes the input stream. */
+    def toBytes(): Array[Byte] = new ByteArrayOutputStream().<|(writeTo).toByteArray
+    /** Closes the input stream. */
+    def writeTo(outputStream: OutputStream): Unit = try {
+      val buffer = new Array[Byte](1 << 14)
       @tailrec
       def go(): Unit = {
         val nRead = $.read(buffer, 0, buffer.length)
@@ -23,7 +60,8 @@ object RichInputStream {
       }
       go()
       outputStream.flush()
-    }
+    } finally $.close()
+    /** Closes the input stream. */
     def writeTo(f: File): Unit = {
       val fos = new FileOutputStream(f)
       try writeTo(fos)
--- src/test/scala/common/rich/RichInputStreamTest.scala
+++ src/test/scala/common/rich/RichInputStreamTest.scala
@@ -2,6 +2,7 @@ package common.rich
 
 import java.io.ByteArrayOutputStream
 
+import org.scalatest.OptionValues.convertOptionToValuable
 import org.scalatest.freespec.AnyFreeSpec
 
 import common.rich.RichInputStream._
@@ -9,8 +10,16 @@ import common.rich.primitives.RichString._
 import common.test.AuxSpecs
 
 class RichInputStreamTest extends AnyFreeSpec with AuxSpecs {
-  "asString" in "foobar".toInputStream.asString.shouldReturn("foobar")
-  "toBytes" in "foobar".toInputStream.toBytes.shouldReturn("foobar".getBytes)
+  "readLine" in {
+    val is = "foo\r\nבאר\n\nbaz".toInputStream
+    is.readLine().value shouldReturn "foo"
+    is.readLine().value shouldReturn "באר"
+    is.readLine().value shouldReturn ""
+    is.readLine().value shouldReturn "baz"
+    is.readLine() shouldBe empty
+  }
+  "asString" in "foobar".toInputStream.asString().shouldReturn("foobar")
+  "toBytes" in "foobar".toInputStream.toBytes().shouldReturn("foobar".getBytes)
   "write to OutputStream" in {
     val baos = new ByteArrayOutputStream()
     "foobar".toInputStream.writeTo(baos)
